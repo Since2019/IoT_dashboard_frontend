81c38622d07de4bbad9f0100b9a885f85b40f027
Rickson2019 <JudaoZhong@gmail.com>
2021-06-12 13:50:54 -0700

    organized the navbar items

--- src/attributes/home-navigation.js
+++ src/attributes/home-navigation.js
@@ -1,7 +1,19 @@
 
 export const nav_options = {
-    Home: "home",
+    Home: "Home",
     Dashboard: "Dashboard",
-
+    Devices: "Devices",
+    Analytics: "Analytics",
+    Sensors: "Sensors",
 }
+
+export const dashboard_options = [
+    { id: 0, name: "Monitor", icon: "monitor" },
+    { id: 1, name: "Analytics", icon: "line-chart" }
+]
+
+// export const dashboard_options = [
+//     { id: 0, name: "Monitor", icon: "monitor" },
+//     { id: 1, name: "Analytics", icon: "line-chart" }
+// ]
 
--- src/components/Home/Navigation.vue
+++ src/components/Home/Navigation.vue
@@ -17,39 +17,75 @@
       :selected-keys="[current]"
       @click="handleClick"
     >
-      <a-menu-item key="1">
-        <a-icon type="mobile" />
-        nav_options.Home
-      </a-menu-item>
-      <a-menu-item key="2">
-        <a-icon type="calendar" />
-        Navigation Two
-      </a-menu-item>
       <a-sub-menu key="sub1">
         <span slot="title"
-          ><a-icon type="appstore" /><span>Navigation Three</span></span
+          ><a-icon type="calendar" /><span>{{
+            nav_options.Dashboard
+          }}</span></span
         >
-        <a-menu-item key="3"> Option 3 </a-menu-item>
-        <a-menu-item key="4"> Option 4 </a-menu-item>
-        <a-sub-menu key="sub1-2" title="Submenu">
-          <a-menu-item key="5"> Option 5 </a-menu-item>
-          <a-menu-item key="6"> Option 6 </a-menu-item>
-        </a-sub-menu>
+
+        <a-menu-item v-for="item in dashboard_options" :key="item.id">
+          <a-icon :type="item.icon" /> {{ item.name }}
+        </a-menu-item>
       </a-sub-menu>
-      <a-sub-menu key="sub2">
+
+      <!-- Devices -->
+      <a-sub-menu key="device-list">
         <span slot="title"
-          ><a-icon type="setting" /><span>Navigation Four</span></span
+          ><a-icon type="setting" /><span>{{ nav_options.Devices }}</span></span
         >
-        <a-menu-item key="7"> Option 7 </a-menu-item>
-        <a-menu-item key="8"> Option 8 </a-menu-item>
-        <a-menu-item key="9"> Option 9 </a-menu-item>
-        <a-menu-item key="10"> Option 10 </a-menu-item>
+
+        <!-- Sensors -->
+        <a-sub-menu key="sensor-list">
+          <!-- title -->
+          <span slot="title">
+            <a-icon type="setting" />
+            <span>
+              {{ nav_options.Sensors }}
+            </span>
+          </span>
+
+          <!-- item v-for -->
+          <a-menu-item v-for="sensor in test_sensors" :key="sensor.id">
+            {{ sensor.name }}
+          </a-menu-item>
+        </a-sub-menu>
+
+        <!-- Smart Devices -->
+        <a-sub-menu key="smart-home-device-list">
+          <!-- title -->
+          <span slot="title">
+            <a-icon type="api" />
+            <span>{{ nav_options.Sensors }}</span>
+          </span>
+
+          <!-- item v-for -->
+          <a-menu-item
+            v-for="smart_device in test_smart_devices"
+            :key="smart_device.id"
+          >
+            {{ smart_device.name }}
+          </a-menu-item>
+        </a-sub-menu>
       </a-sub-menu>
     </a-menu>
   </div>
 </template>
 <script>
-import { nav_options } from "@/attributes/home-navigation.js";
+import {
+  nav_options,
+  dashboard_options,
+} from "@/attributes/home-navigation.js";
+
+const test_smart_devices = [
+  { id: "smart_devices1", name: "Device1" },
+  { id: "smart_devices2", name: "Device2" },
+];
+
+const test_sensors = [
+  { id: "sensor1", name: "DHT11" },
+  { id: "sensor2", name: "BMP280" },
+];
 
 export default {
   data() {
@@ -57,6 +93,10 @@ export default {
       current: "1",
       theme: "dark",
       nav_options: nav_options,
+      dashboard_options: dashboard_options,
+      // test
+      test_sensors: test_sensors,
+      test_smart_devices: test_smart_devices,
     };
   },
   methods: {
