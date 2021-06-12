4cc79bacb49bb5228c096d8700a55a2ba8c27aee
Rickson2019 <JudaoZhong@gmail.com>
2021-06-12 12:52:40 -0700

    first commit

---
+++ src/attributes/home-navigation.js
@@ -0,0 +1,7 @@
+
+export const nav_options = {
+    Home: "home",
+    Dashboard: "Dashboard",
+
+}
+
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Vue App made by JudaoZhong</h1>
+    <h2>Internet of Things Project</h2>
+  </div>
+</template>
